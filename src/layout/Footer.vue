3a2e972e9c170c68eeb5b78ed1c3b61d2a889829
NBDatsuya <2646809050@qq.com>
2024-03-12 19:52:25 +0800

    init commit

---
+++ src/layout/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    我来组成尾部
+</template>
+
+<style scoped>
+</style>
---
+++ src/layout/SideBar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    我来组成侧栏
+</template>
+
+<style scoped>
+</style>
