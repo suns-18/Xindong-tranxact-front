c96df402dffa72eb44fe559902a12c57d4d3abea
NBDatsuya <2646809050@qq.com>
2024-04-25 18:41:41 +0800

    dependencies: pinia, mdi-icon

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "@/ts/router.ts";
+import pinia from "@/ts/pinia.ts";
+import "@mdi/font/css/materialdesignicons.min.css"
 
+// @ts-ignore
 createApp(App)
     .use(router)
+    .use(pinia)
     .mount('#app')
