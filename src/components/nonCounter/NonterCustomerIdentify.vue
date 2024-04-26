776b7910021417747d5a77f21ee135d93df2f1ba
NBDatsuya <2646809050@qq.com>
2024-04-27 00:00:30 +0800

    fix: position.shareTotal

--- src/components/nonCounter/NonterCustomerIdentify.vue
+++ src/components/nonCounter/NonterCustomerIdentify.vue
@@ -292,8 +292,7 @@ const commission = ref<CommissionRecord[]>([])
                             <td data-th="股份余额"
                                 class="before:w-24 before:inline-block before:font-medium before:text-slate-700 before:content-[attr(data-th)':'] sm:before:content-none flex items-center sm:table-cell h-12 px-2 text-sm transition duration-300 sm:border-t sm:border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
                                 {{
-                                    (item.marketInfo.currentPrice *
-                                      item.marketInfo.availableQuantity).toFixed(4)
+                                    item.position.shareTotal
                                 }}
                             </td>
                             <td data-th="股份冻结数量"
